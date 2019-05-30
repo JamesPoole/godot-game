cd5512ce99df0a75117a88e92fee141dda358c79
James Poole <jgaple@gmail.com>
2019-05-29 21:26:13 -0700

    add basic interactions

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const ACCEL = 50
-const MAX_SPEED = 200
+const ACCEL = 40
+const MAX_SPEED = 125
 const JUMP_HEIGHT = -200
 
 var motion = Vector2()
@@ -16,3 +16,4 @@ func _physics_process(delta):
 	var friction = false
 	
 	motion = move_and_slide(motion, UP)
+
--- Characters/Hero/Hero.gd
+++ Characters/Hero/Hero.gd
@@ -1,10 +1,20 @@
 extends "res://Characters/Character.gd"
 
+var can_interact = false
+
 func _ready():
-	pass
+	var hero = get_node(".")
+	var interactables = get_tree().get_nodes_in_group("Interactable")
+	for i in range(interactables.size()):
+		var currNode = get_node(interactables[i].get_path())
+		var area2DNode = currNode.get_node("Area2D")
+		var args = Array([currNode])
+		area2DNode.connect("body_enter", hero, "_on_Area2D_body_enter", args)
+		area2DNode.connect("body_exit", hero, "_on_Area2D_body_exit", args)
 
 #warning-ignore:unused_argument
 func _physics_process(delta):
+	#basic movement
 	if Input.is_action_pressed("ui_right"):
 		motion.x += ACCEL
 		motion.x = min(motion.x, MAX_SPEED)
@@ -13,7 +23,21 @@ func _physics_process(delta):
 		motion.x = max(motion.x, -MAX_SPEED)
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
-
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	
+	#interaction
+	if can_interact == true:
+		if Input.is_action_just_pressed("interact"):
+			print('hi')
+
+#on colliding with another interactable, allow interactions
+func _on_Area2D_area_entered(area):
+	if (area.get_parent().get_name() != "Hero"):
+		can_interact = true
+
+#on leaving another interactable, disallow interactions
+func _on_Area2D_area_exited(area):
+	if (area.get_parent().get_name() != "Hero"):
+		can_interact = false
