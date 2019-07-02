b1909cd8a7ebc7e36f002d3269f0e008f08e5b91
James Poole <jgaple@gmail.com>
2019-07-02 14:24:37 -0700

    make title screen the default

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -2,8 +2,13 @@ extends Control
 
 func _ready():
 	pass
-	
+
+func start_game():
+	get_tree().change_scene('res://game.tscn')
+
 func _physics_process(delta):
 	if Input.is_action_just_released("interact"):
-		print('pressed')
-		get_tree().change_scene('res://game.tscn')
+		start_game()
+
+func _on_A_Button_button_up():
+	start_game()
